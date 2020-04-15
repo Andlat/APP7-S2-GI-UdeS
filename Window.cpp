22fe3b492908bfab493176754b04d0813653dbcd
lb-udes <55243135+lb-udes@users.noreply.github.com>
2020-04-15 10:48:20 -0400

    modification de l'aide

--- Window.cpp
+++ Window.cpp
@@ -167,12 +167,12 @@ void Window::help()
 {
 	QString text;
 	text = ("Ce jeu consiste a remporter de l'argent a chaque fois que vous trouvez la bonne reponse aux questions. Pour vous aidez,"
-		"vous disposez de 4 outils soit : le 50/50; le telephone; l'avis du public et le joker. Leur fonctionnement sont expliques ci-dessous"
-		"Le 50/50 permet d'eliminer 2 reponses sur les 4. La reponse de la question se trouve dans l'une de ces 2 reponses"
-		"Le telephone vous donne la possibilite d'appeler un ami qui pourra vous aider a repondre a la question"
-		"L'aide du public vous permet de consulter le public et d'avoir leur opinion sur reponse"
-		"Le joker vous donne directement la bonne reponse "
+		"vous disposez de 3 outils soit : le 50/50, le telephone et l'avis du public. Leur fonctionnement sont expliques ci-dessous :\n"
+		"  -Le 50/50 permet d'eliminer 2 reponses sur les 4. La reponse de la question se trouve dans l'une de ces 2 reponses.\n"
+		"  -Le telephone vous donne la possibilite d'appeler un ami qui pourra vous aider a repondre a la question.\n"
+		"  -L'aide du public vous permet de consulter le public et d'avoir leur opinion sur reponse.\n\n"
 		"Bonne chance :) ");
+    //text = text.leftJustified(120, ' ');
 	messageBox(text, "Aide");
 }
 
